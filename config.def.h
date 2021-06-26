22042221cf5db39346f0a0e622a0d1a1b7db5cd5
komildeveloper <komildeveloper111@gmail.com>
2021-06-27 01:52:40 +0500

    updated config

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static char *font2[] = {
 	"Hack Nerd Font Mono:pixelsize=20:antialias=true:autohint=true",
 };
 
-static int borderpx = 0;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
