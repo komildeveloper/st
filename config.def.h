4af326a9486773290615f6a8645ad533f08c341b
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2021-08-13 15:25:10 +0500

    update st

--- config.def.h
+++ config.def.h
@@ -5,13 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:style=Medium:pixelsize=20:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-	"Hack Nerd Font Mono:pixelsize=20:antialias=true:autohint=true",
+static char *font = "monospace:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { 
+    "Braille:pixelsize=11:antialias=true:autohint=true"
+    /* "JoyPixels:pixelsize=11:antialias=true:autohint=true", */
 };
-
 static int borderpx = 1;
 
 /*
